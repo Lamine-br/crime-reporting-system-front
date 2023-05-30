2aa1e2b5ee50bdef34ea767ad790be7cc9d771f7
aghiles23 <aghilesbelkacem123@gmail.com>
2023-05-30 02:36:53 +0200

    page investigation amelioree

--- src/pages/InvestigationDetails.js
+++ src/pages/InvestigationDetails.js
@@ -5,33 +5,31 @@ import { Button } from "@mui/material";
 import Iconify from "../components/iconify";
 
 export function InvestigationDetails() {
-	let generalDetails = {
-		idReport: "1",
-		openingDate: "11/02/2002",
-		closingDate: "11/02/2002",
-		openingTime: "9.12",
-		closingTime: "10.30",
-		status: "Ongoing",
-		idCitizen: "Lamine",
-	};
+  let generalDetails = {
+    idReport: "1",
+    openingDate: "11/02/2002",
+    closingDate: "11/02/2002",
+    openingTime: "9.12",
+    closingTime: "10.30",
+    status: "Ongoing",
+    idCitizen: "Lamine",
+  };
 
-	let conclusion = "Yekfa laman, tura dayen, tekfa niya deg ulawen";
+  let conclusion = "Yekfa laman, tura dayen, tekfa niya deg ulawen";
 
-	return (
-		<div className='bg-gray-200 w-full px-10 py-6 flex flex-col justify-center items-center rounded-2xl'>
-			<h3 className='text-2xl font-semibold text-center'>
-				Investigation Details
-			</h3>
-			<GeneralDetails data={generalDetails}></GeneralDetails>
-			<FactsFindings></FactsFindings>
-			<Conclusion data={conclusion}></Conclusion>
-			<Button
-				className='w-1/6'
-				variant='contained'
-				startIcon={<Iconify icon='feather:save' />}
-			>
-				Save
-			</Button>
-		</div>
-	);
+  return (
+    <div className="bg-gray-100 w-full px-10 py-6 flex flex-col justify-center items-center rounded-2xl">
+      <h3 className="text-3xl font-bold text-center">Investigation Details</h3>
+      <GeneralDetails data={generalDetails}></GeneralDetails>
+      <FactsFindings></FactsFindings>
+      <Conclusion data={conclusion}></Conclusion>
+      <Button
+        className="w-1/6"
+        variant="contained"
+        startIcon={<Iconify icon="feather:save" />}
+      >
+        Save
+      </Button>
+    </div>
+  );
 }
